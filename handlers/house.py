626925e7e5ee57eb6fda7862d594519d22b73765
liangoy <emailboxbeta@gmail.com>
2018-07-12 11:16:25 +0800

    cunc

--- handlers/house.py
+++ handlers/house.py
@@ -26,14 +26,7 @@ def price_predicted(**kwargs):
 def around_info(**kwargs):
     location = kwargs['location']
     radius = kwargs['radius']
-    info = {
-        'banks': gaode_service.around(key_word='银行', location=location, radius=radius),
-        'schools': gaode_service.around(key_word='学校', location=location, radius=radius),
-        'hospitals': gaode_service.around(key_word='医院', location=location, radius=radius),
-        'hotels': gaode_service.around(key_word='宾馆', location=location, radius=radius),
-        'bus_stops': gaode_service.around(key_word='公交站', location=location, radius=radius),
-        'subway_stations': gaode_service.around(key_word='地铁站', location=location, radius=radius),
-    }
+    info=gaode_service.around(key_words='银行,学校,医院,宾馆,公交站,地铁站',location=location,radius=radius)
     for i in info:
         info[i] = str(info[i])
     return info
--- services/gaode.py
+++ services/gaode.py
@@ -1,13 +1,15 @@
 import config
 import requests
+import grequests
 from utils import gaode
+import json
 class Gaode_service():
     key=config.GAODE_KEY
     def add_sign(self,par):
         par['key']=self.key
         par['sign'] = gaode.encrypt(par=par, key=self.key)
         return par
-    def address2location(self,address=None,city=None,batch='false',output='JSON',callback=None):
+    def address2location(self,address=None,city=None,batch='false',output='JSON',concurrent=1):
         url='http://restapi.amap.com/v3/geocode/geo?parameters'
         par={
             'address':address,
@@ -20,27 +22,32 @@ class Gaode_service():
         res=r.json()
         return res['geocodes'][0]['formatted_address'],res['geocodes'][0]['location']
 
-    def around(self,key_word=None,location=None,radius='3000',types = None,sortrule='distance',output='JSON'):
+    def around(self,key_words=None,location=None,radius='3000',types = None,sortrule='distance',output='JSON'):
         url='http://restapi.amap.com/v3/place/around?parameters'
-        par={
-            'keywords':key_word,
-            'location':location,
-            'sortrule':sortrule,
-            'radius':radius,
-            'output':output,
-            'offset':'20',
-            'page':'1',
-            'types':types
-        }
-        for i in list(par.keys()):
-            if par[i]==None:
-                par.pop(i)
+        par_list=[]
+        for key_word in key_words.split(','):
+            par={
+                'keywords':key_word,
+                'location':location,
+                'sortrule':sortrule,
+                'radius':radius,
+                'output':output,
+                'offset':'20',
+                'page':'1',
+                'types':types
+            }
+            for i in list(par.keys()):
+                if par[i]==None:
+                    par.pop(i)
 
-        par = self.add_sign(par)
-        r = requests.get(url, params=par)
-        r.close()
-        res = r.json()
-        return res['count']
+            par = self.add_sign(par)
+            par_list.append(par)
+        tasks=[grequests.get(url,params=i) for i in par_list]
+        resp=list(zip(key_words.split(','),grequests.map(tasks)))
+        for r in resp:
+            r[1].close()
+        return {i[0]:json.loads(i[1].text)['count'] for i in resp}
+
 
 gaode_service=Gaode_service()
 if __name__=='__main__':
@@ -52,4 +59,4 @@ if __name__=='__main__':
     #addr='广东省深圳市南山区南油南光路65-22号'
     #addr = '深圳南山华侨城沙河东路186号深圳湾畔花园D栋8091厨2室1阳台1卫1厅|广东省-深圳市-深圳湾畔花园-1厨2室1阳台1卫1厅'
     pprint(gs.address2location(address=addr))
-    #pprint(gs.around(location=gs.address2location(address=addr)[1],key_word='学校'))
+    pprint(gs.around(location=gs.address2location(address=addr)[1],key_words='学校'))
